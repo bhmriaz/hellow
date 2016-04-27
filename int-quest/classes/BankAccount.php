1794e7671db4c017148c50a809190ad13c72ee81
bhmriaz <musabin520@gmail.com>
2016-04-27 16:37:25 +0600

    just upload
    nothing to say

---
+++ int-quest/classes/BankAccount.php
@@ -0,0 +1,92 @@
+<?php
+
+class BankAccount implements IfaceBankAccount  
+{
+
+    private $balance = null;
+	 
+    public function __construct(Money $openingBalance)
+    {
+        $this->balance = $openingBalance;
+		 
+    }
+
+    public function balance()
+    {
+        return  $this->balance;
+    }
+
+    public function deposit(Money $amount)
+    {
+		
+		 $this->balance =   $amount->amount + ($this->balance->amount);
+		
+		return   $this->balance;
+    }
+	public function withdraw(Money $amount)
+	{	
+		 
+		 
+		if (is_object($this->balance))
+		{
+		             
+		if($this->balance->amount > $amount->value()){
+		$this->balance = ($this->balance->amount) - $amount->value();
+
+		}
+		else{
+		 
+		 throw new Exception('Withdrawl amount larger than balance');
+		
+		 
+		}
+	}else{
+ 
+		 if($this->balance  > $amount->value()){
+		$this->balance = $this->balance  - $amount->value();
+ 
+		}
+		else{
+		 throw new Exception('Withdrawl amount larger than balance');
+		}
+ 
+		}
+ 
+	  
+		
+	}
+	
+    public function transfer(Money $amount, BankAccount $account)
+    {
+	
+	 
+	$accounts = $this->balance;
+	 
+	
+	 
+	  if($accounts > $amount->value())
+	  { 
+	  
+		$this->balance =  $accounts - (int)$amount->value();
+		$account->balance = $account->balance + (int)$amount->value();
+		return $account->balance;
+		  
+	  }
+	  else{
+	 
+	  throw new Exception('Withdrawl amount larger than balance');
+	  
+	  }
+	  
+
+	 
+    }
+ 
+ 
+	 
+	 
+ 
+ 
+	
+	
+}
---
+++ int-quest/classes/Money.php
@@ -0,0 +1,24 @@
+<?php
+
+class Money
+{
+    public $amount = 0;
+ 
+    public function __construct($amount)
+    {
+        $this->amount = (float) $amount;
+    }
+
+    public function value()
+    {
+        return $this->amount;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->value();
+    }
+	
+	 
+	
+}
